c795cc44aa1c02a59d3b108c64c720333f815f57
Maxim VT <maxim.vt@gmail.com>
2019-04-05 22:57:56 +0300

    code clean

--- CalcYandex/Program.cs
+++ CalcYandex/Program.cs
@@ -7,195 +7,195 @@ using System;
 /// </summary>
 namespace CalcYandex
 {
-	class Program
+    class Program
     {
-		/// <summary>
-		/// array of costs each symbol
-		/// </summary>
+        /// <summary>
+        /// array of costs each symbol
+        /// </summary>
         private static readonly int[] cost_array = new int[10]
         {
         //  0  1  2  3  4  5  6  7  8  9
             6, 2, 5, 5, 4, 5, 6, 3, 7, 6
         };
 
-		/// <summary>
-		/// Array for minimal number
-		/// </summary>
+        /// <summary>
+        /// Array for minimal number
+        /// </summary>
         private static int[] arr_min;
 
-		/// <summary>
-		/// Array for maximal number
-		/// </summary>
+        /// <summary>
+        /// Array for maximal number
+        /// </summary>
         private static int[] arr_max;
 
-		static void Main(string[] args)
-		{
-			do
-			{
-				#region Prepare Console
+        static void Main(string[] args)
+        {
+            do
+            {
+                #region Prepare Console
 
-				Console.ForegroundColor = ConsoleColor.Green;
+                Console.ForegroundColor = ConsoleColor.Green;
 
-				Console.WriteLine("Any key - Run task\n" +
-				  "Esc - Exit\n");
-				ConsoleKeyInfo key = Console.ReadKey();
-				Console.WriteLine();
-				
-				#endregion
+                Console.WriteLine("Any key - Run task\n" +
+                  "Esc - Exit\n");
+                ConsoleKeyInfo key = Console.ReadKey();
+                Console.WriteLine();
 
-				switch (key.Key)
-				{
-					case ConsoleKey.Escape:
-						return;
-					default:
+                #endregion
 
-						{
-							#region Input data
-							
-							var n = RequestIntValue("Please type N:");
+                switch (key.Key)
+                {
+                    case ConsoleKey.Escape:
+                        return;
+                    default:
 
-							var k = RequestIntValue("Please type K:");
-							
-							var startTime = new DateTime();
-							var endTime = new DateTime();
-							startTime = DateTime.Now;
+                        {
+                            #region Input data
 
-							Console.WriteLine($"N: {n}; K: {k}\nStart time: {startTime}");
+                            var n = RequestIntValue("Please type N:");
 
-							#endregion
+                            var k = RequestIntValue("Please type K:");
 
-							#region Check if NO SOLUTION
+                            var startTime = new DateTime();
+                            var endTime = new DateTime();
+                            startTime = DateTime.Now;
 
-							// Check if no solution and break
-							if (( n * 2 > k ) | ( n * 7 < k ) | n < 0 | k < 0)
-							{
-								Console.WriteLine("NO SOLUTION");
-								Console.ReadKey();
-								Console.Clear();
-								break;
-							}
+                            Console.WriteLine($"N: {n}; K: {k}\nStart time: {startTime}");
 
-							#endregion
+                            #endregion
 
-							#region Prepare arrays
+                            #region Check if NO SOLUTION
 
-							// Set min value of number
-							arr_min = new int[n];
-							for (int i = 1; i < n; i++)
-								arr_min[i] = 0;
-							arr_min[0] = 1;
+                            // Check if no solution and break
+                            if (( n * 2 > k ) | ( n * 7 < k ) | n < 0 | k < 0)
+                            {
+                                Console.WriteLine("NO SOLUTION");
+                                Console.ReadKey();
+                                Console.Clear();
+                                break;
+                            }
 
-							// Set sum of black lines
-							int sum_min = n * 6 - 4;
+                            #endregion
 
-							// Set Max value of number
-							arr_max = new int[n];
-							for (int i = 0; i < n; i++)
-								arr_max[i] = 9;
+                            #region Prepare arrays
 
-							// Set sum of black lines
-							int sum_max = n * 6;
+                            // Set min value of number
+                            arr_min = new int[n];
+                            for (int i = 1; i < n; i++)
+                                arr_min[i] = 0;
+                            arr_min[0] = 1;
 
-							#endregion
+                            // Set sum of black lines
+                            int sum_min = n * 6 - 4;
 
-							#region Calculation
+                            // Set Max value of number
+                            arr_max = new int[n];
+                            for (int i = 0; i < n; i++)
+                                arr_max[i] = 9;
 
-							MinNumber(n - 1, sum_min, k);
-							MaxNumber(n - 1, sum_max, k);
+                            // Set sum of black lines
+                            int sum_max = n * 6;
 
-							#endregion
+                            #endregion
 
-							#region Output data
+                            #region Calculation
 
-							endTime = DateTime.Now;
-							Console.WriteLine($"Elapsed Time: {endTime - startTime}");
+                            MinNumber(n - 1, sum_min, k);
+                            MaxNumber(n - 1, sum_max, k);
 
-							Console.WriteLine("Min value is:");
-							for (int i = 0; i < n; i++)
-							{
-								Console.Write($"{arr_min[i]} ");
-							}
-							Console.WriteLine();
+                            #endregion
 
-							Console.WriteLine("Max value is:");
-							for (int i = 0; i < n; i++)
-							{
-								Console.Write($"{arr_max[i]} ");
-							}
-							Console.WriteLine();
+                            #region Output data
 
-							Console.ReadKey();
-							Console.Clear();
+                            endTime = DateTime.Now;
+                            Console.WriteLine($"Elapsed Time: {endTime - startTime}");
 
-							#endregion
-						}
-						break;
-				}
-			} while (true);
+                            Console.WriteLine("Min value is:");
+                            for (int i = 0; i < n; i++)
+                            {
+                                Console.Write($"{arr_min[i]} ");
+                            }
+                            Console.WriteLine();
 
-		}
+                            Console.WriteLine("Max value is:");
+                            for (int i = 0; i < n; i++)
+                            {
+                                Console.Write($"{arr_max[i]} ");
+                            }
+                            Console.WriteLine();
 
-		/// <summary>
-		/// Input value and convert them from Str to Int
-		/// repeat until value become integer and can be converted
-		/// </summary>
-		/// <param name="message">string incoming from programm</param>
-		/// <returns>integer value number</returns>
-		public static int RequestIntValue(string message)
-		{
-			int number;
+                            Console.ReadKey();
+                            Console.Clear();
 
-			Console.ForegroundColor = ConsoleColor.Red;
-			do
-			{ Console.WriteLine(message); }
-			while (!Int32.TryParse(Console.ReadLine(), out number));
+                            #endregion
+                        }
+                        break;
+                }
+            } while (true);
 
-			Console.ForegroundColor = ConsoleColor.Green;
-			return number;
-		}
+        }
 
-		/// <summary>
-		/// Method Update value and Calculate new sum
-		/// </summary>
-		/// <param name="value">new value</param>
-		/// <param name="cursor">current position in array</param>
-		/// <param name="sum">current sum</param>
-		/// <param name="arr">current array</param>
-		/// <returns>new sum of black lines</returns>
-		static int UpdateValue(int value, int cursor, int sum, int[] arr)
-			{
-			sum = sum + cost_array[value] - cost_array[arr[cursor]];
-			arr[cursor] = value;
-			return sum;
-			}
+        /// <summary>
+        /// Input value and convert them from Str to Int
+        /// repeat until value become integer and can be converted
+        /// </summary>
+        /// <param name="message">string incoming from programm</param>
+        /// <returns>integer value number</returns>
+        public static int RequestIntValue(string message)
+        {
+            int number;
 
-		/// <summary>
-		/// Method find Minimal Number with requested sum of black lines
-		/// </summary>
-		/// <param name="cursor">current position in array</param>
-		/// <param name="sum">current sum</param>
-		/// <param name="k">requested sum</param>
+            Console.ForegroundColor = ConsoleColor.Red;
+            do
+            { Console.WriteLine(message); }
+            while (!Int32.TryParse(Console.ReadLine(), out number));
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            return number;
+        }
+
+        /// <summary>
+        /// Method Update value and Calculate new sum
+        /// </summary>
+        /// <param name="value">new value</param>
+        /// <param name="cursor">current position in array</param>
+        /// <param name="sum">current sum</param>
+        /// <param name="arr">current array</param>
+        /// <returns>new sum of black lines</returns>
+        static int UpdateValue(int value, int cursor, int sum, int[] arr)
+        {
+            sum = sum + cost_array[value] - cost_array[arr[cursor]];
+            arr[cursor] = value;
+            return sum;
+        }
+
+        /// <summary>
+        /// Method find Minimal Number with requested sum of black lines
+        /// </summary>
+        /// <param name="cursor">current position in array</param>
+        /// <param name="sum">current sum</param>
+        /// <param name="k">requested sum</param>
         static void MinNumber(int cursor, int sum, int k)
         {
             while (sum > k)
-				sum = UpdateValue(1, cursor--, sum, arr_min);
+                sum = UpdateValue(1, cursor--, sum, arr_min);
 
-			cursor = arr_min.Length - 1;
+            cursor = arr_min.Length - 1;
             while (sum < k)
             {
                 switch (arr_min[cursor])
                 {
                     case 0:
-					case 6:
-					case 9:
-						sum = UpdateValue(8, cursor--, sum, arr_min);
+                    case 6:
+                    case 9:
+                        sum = UpdateValue(8, cursor--, sum, arr_min);
                         break;
-                    case 1:              
+                    case 1:
                         sum = UpdateValue(7, cursor, sum, arr_min);
                         break;
                     case 2:
-					case 3:
-					case 5:
+                    case 3:
+                    case 5:
                         sum = UpdateValue(0, cursor, sum, arr_min);
                         break;
                     case 4:
@@ -211,39 +211,39 @@ namespace CalcYandex
             }
         }
 
-		/// <summary>
-		/// Method find Maximal Number with requested sum of black lines
-		/// </summary>
-		/// <param name="cursor">current position in array</param>
-		/// <param name="sum">current sum</param>
-		/// <param name="k">requested sum</param>
-		static void MaxNumber(int cursor, int sum, int k)
+        /// <summary>
+        /// Method find Maximal Number with requested sum of black lines
+        /// </summary>
+        /// <param name="cursor">current position in array</param>
+        /// <param name="sum">current sum</param>
+        /// <param name="k">requested sum</param>
+        static void MaxNumber(int cursor, int sum, int k)
         {
-			while (sum > k)
-			{
-				sum = UpdateValue(1, cursor, sum, arr_max);
-				if (sum > k) cursor--;
-			}
+            while (sum > k)
+            {
+                sum = UpdateValue(1, cursor, sum, arr_max);
+                if (sum > k) cursor--;
+            }
 
             while (sum < k & cursor < arr_max.Length)
             {
                 switch (arr_max[cursor])
                 {
                     case 0:
-					case 6:
-					case 9:
-						sum = UpdateValue(8, cursor++, sum, arr_max);                        
+                    case 6:
+                    case 9:
+                        sum = UpdateValue(8, cursor++, sum, arr_max);
                         break;
                     case 1:
-						sum = UpdateValue(7, cursor, sum, arr_max);
+                        sum = UpdateValue(7, cursor, sum, arr_max);
                         break;
                     case 2:
-					case 3:
-					case 5:
-						sum = UpdateValue(9, cursor, sum, arr_max);
+                    case 3:
+                    case 5:
+                        sum = UpdateValue(9, cursor, sum, arr_max);
                         break;
                     case 4:
-						sum = UpdateValue(5, cursor, sum, arr_max);
+                        sum = UpdateValue(5, cursor, sum, arr_max);
                         break;
                     case 7:
                         sum = UpdateValue(4, cursor, sum, arr_max);
@@ -254,34 +254,34 @@ namespace CalcYandex
                 }
             }
 
-			cursor = arr_max.Length - 1;
-			while (sum < k)
+            cursor = arr_max.Length - 1;
+            while (sum < k)
             {
-				switch (arr_max[cursor])
-				{
-					case 0:
-					case 6:
-					case 9:
-						sum = UpdateValue(8, cursor--, sum, arr_max);
-						break;
-					case 1:
-						sum = UpdateValue(7, cursor, sum, arr_max);
-						break;
-					case 2:
-					case 3:
-					case 5:
-						sum = UpdateValue(9, cursor, sum, arr_max);
-						break;
-					case 4:
-						sum = UpdateValue(5, cursor, sum, arr_max);
-						break;
-					case 7:
-						sum = UpdateValue(4, cursor, sum, arr_max);
-						break;
-					case 8:
-						cursor--;
-						break;
-				}
+                switch (arr_max[cursor])
+                {
+                    case 0:
+                    case 6:
+                    case 9:
+                        sum = UpdateValue(8, cursor--, sum, arr_max);
+                        break;
+                    case 1:
+                        sum = UpdateValue(7, cursor, sum, arr_max);
+                        break;
+                    case 2:
+                    case 3:
+                    case 5:
+                        sum = UpdateValue(9, cursor, sum, arr_max);
+                        break;
+                    case 4:
+                        sum = UpdateValue(5, cursor, sum, arr_max);
+                        break;
+                    case 7:
+                        sum = UpdateValue(4, cursor, sum, arr_max);
+                        break;
+                    case 8:
+                        cursor--;
+                        break;
+                }
             }
         }
     }
